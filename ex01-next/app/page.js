4657056a559fcaabe94c1f113e8e10d8dd871c65
ygortsf <ygordetarcio@gmail.com>
2025-08-15 00:06:59 +0000

    commit de hj

--- ex01-next/app/page.js
+++ ex01-next/app/page.js
@@ -1,10 +1,14 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+// import Image from "next/image";
+// import styles from "./page.module.css";
+
+import{ Profile } from  "@/components"
+
 
 export default function Home() {
   return (
     <div>
       iae mamae
+      <Profile/>
      </div>
   )
 }
