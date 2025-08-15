19fdf7d55a7a7662d9bc97cb5311f663a8503f78
ygortsf <ygordetarcio@gmail.com>
2025-08-15 00:25:30 +0000

    little monster

--- ex01-next/components/Profile.js
+++ ex01-next/components/Profile.js
@@ -1,9 +1,8 @@
 export function Profile() {
   return (
     <img
-      src="https://i.imgur.com/MK3eW3As.jpg"
+      src="https://ichef.bbci.co.uk/ace/ws/640/cpsprodpb/5f52/live/27ae3b90-fad6-11ef-a76c-f7e6d97500f1.jpg.webp"
       alt="Katherine Johnson"
     />
   );
 }
-
